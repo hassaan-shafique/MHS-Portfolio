eea8d3cf3bce8220c48355fba237b9922708c55d
hassaan-shafique <hassaanshafique398@gmail.com>
2024-03-13 15:51:01 +0500

    header links

--- src/Components/header.js
+++ src/Components/header.js
@@ -4,16 +4,16 @@ const header = () => {
   return (
     <div>
       <header className="header">
-        
-          <h1> MHS Portfolio</h1>
-        
+        <h1> MHS Portfolio</h1>
 
-        <a href="/"> Home</a>
-        <a href="/Introduction"> Introduction</a>
-        <a href="/Skills"> Skills</a>
-        <a href="/Projects"> Projects</a>
-        <a href="/Education"> Education</a>
-        <a href="/Certifications"> Certifications</a>
+        <div className="links">
+          <a href="/"> Home</a>
+          <a href="/Introduction"> Introduction</a>
+          <a href="/Skills"> Skills</a>
+          <a href="/Projects"> Projects</a>
+          <a href="/Education"> Education</a>
+          <a href="/Certifications"> Certifications</a>
+        </div>
       </header>
     </div>
   );
