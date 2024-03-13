08d3b7ba835e15aaf25b6d4a5cb267fc81cc9e96
hassaan-shafique <hassaanshafique398@gmail.com>
2024-03-13 15:05:42 +0500

    header

---
+++ src/Components/header.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const header = () => {
+  return (
+    <div>
+      <header className="header">
+        
+        <link> Home</link>
+        <link> Introduction</link>
+        <link> Skills</link>
+        <link> Projects</link>
+        <link> Education</link>
+        <link> Certifications</link>
+      </header>
+    </div>
+  );
+}
+
+export default header
