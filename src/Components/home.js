d5662b3c40223c9ad5b2b500b7a6967040052241
hassaan-shafique <hassaanshafique398@gmail.com>
2024-03-16 03:38:41 +0500

    home

--- src/Components/home.js
+++ src/Components/home.js
@@ -9,13 +9,7 @@ const Home = () => {
         Hello, I am Muhammad Hassaan Shafique <br />
         I'm a Software Engineer
       </h1>
-      
-      <div className="icons">
-        <img src="instagram.png" alt="Icon 1" />
-        <img src="facebook.png" alt="Icon 2" />
-        <img src="linkedin.png" alt="Icon 3" />
-        <img src="twitter (1).png" alt="Icon 4" />
-      </div>
+    
     </div>
   );
 }
