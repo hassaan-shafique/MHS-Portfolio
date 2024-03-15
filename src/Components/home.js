353d92abe618a0382609045b0757384c0ad3fc24
hassaan-shafique <hassaanshafique398@gmail.com>
2024-03-16 03:30:27 +0500

    home

---
+++ src/Components/home.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+
+
+const Home = () => {
+  return (
+    <div>
+      <h1 className="home-intro">
+        Hello, I am Muhammad Hassaan Shafique <br />
+        I'm a Software Engineer
+      </h1>
+      
+      <div className="icons">
+        <img src="instagram.png" alt="Icon 1" />
+        <img src="facebook.png" alt="Icon 2" />
+        <img src="linkedin.png" alt="Icon 3" />
+        <img src="twitter (1).png" alt="Icon 4" />
+      </div>
+    </div>
+  );
+}
+
+export default Home
