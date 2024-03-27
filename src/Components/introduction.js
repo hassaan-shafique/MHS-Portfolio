d8c16353ad99509e445489e31da747a184a3c396
hassaan-shafique <hassaanshafique398@gmail.com>
2024-03-27 16:45:28 +0500

    intro

--- src/Components/introduction.js
+++ src/Components/introduction.js
@@ -3,8 +3,24 @@ import React from 'react'
 const Introduction = () => {
   return (
     <div>
-      <div className='intro-heading'>
-        <h1>Introduction</h1>
+      
+      <div className='About-img'>
+        <img 
+        width={250} 
+        height={460} 
+        src="image3.png" alt="About me picture" />
+      </div>
+
+      <div className='introduction'>
+      <h2>About <span >Me </span></h2>
+      <div className='intro-description'> 
+      I am a full Stack Developer with a 1 year Experience. i did an Internship in CARE<br/>
+       where my job was to in Front-end Currently 
+      i am working as a Software Engineer <br/>at Ivory Solutions at Kickstart Islamabad, Pakistan.
+      </div>
+
+
+
       </div>
     </div>
   );
