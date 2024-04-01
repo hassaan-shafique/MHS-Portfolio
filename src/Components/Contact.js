caa75de4238272561feffda5bf92bb22d5cc9110
hassaan-shafique <hassaanshafique398@gmail.com>
2024-04-02 03:26:46 +0500

    contact

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -4,7 +4,7 @@ const Contact = () => {
   return (
     <section id="contact">
       <div>
-        <h1> Contact </h1>
+        <h1 className='contact-head'> Contact </h1>
         <div className="formm">
           <form>
             {/* <label>Name:</label> */}
