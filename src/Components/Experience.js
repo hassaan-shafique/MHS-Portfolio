03dfe9daca68fe9973b8cf4359269df442adacae
hassaan-shafique <hassaanshafique398@gmail.com>
2024-04-05 02:55:19 +0500

    experince

--- src/Components/Experience.js
+++ src/Components/Experience.js
@@ -4,8 +4,14 @@ const Experience = () => {
   return (
     <div>
       <h1>Experience </h1>
+      <div className="main-experience">
+        <div className="Ivory-solutions"></div>
+        <div className="Care"></div>
+        <div className="work"></div>
+        <div className="shark-technology"></div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Experience
