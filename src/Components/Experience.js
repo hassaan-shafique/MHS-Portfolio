3b1f33f5e04dea0611d269f97219ec9203419718
hassaan-shafique <hassaanshafique398@gmail.com>
2024-04-18 18:47:18 +0500

    ul

--- src/Components/Experience.js
+++ src/Components/Experience.js
@@ -11,19 +11,66 @@ const Experience = () => {
           <div className="company-location">
             <div className="company">Ivory Solutions</div>
             <div className="location">Islamabad,Pakistan</div>
-            <div className="roles">
-              <ul>
-                <li>My first Role</li>
-                <li>second </li>
-                <li></li>
-                <li></li>
-              </ul>
-            </div>
+          </div>
+          <div className="job-roles">
+            <ul>
+              <li>My first Role</li>
+              <li>second </li>
+              <li>third</li>
+              <li>Fourth</li>
+            </ul>
           </div>
         </div>
-        <div className="Care"></div>
-        <div className="work"></div>
-        <div className="shark-technology"></div>
+        <div className="Care">
+          {" "}
+          <div className="date">Feb-2024--Present</div>
+          <div className="role">Front-end Developer</div>
+          <div className="company-location">
+            <div className="company">Ivory Solutions</div>
+            <div className="location">Islamabad,Pakistan</div>
+          </div>
+          <div className="job-roles">
+            <ul>
+              <li>My first Role</li>
+              <li>second </li>
+              <li>third</li>
+              <li>Fourth</li>
+            </ul>
+          </div>{" "}
+        </div>
+        <div className="work">
+          {" "}
+          <div className="date">Feb-2024--Present</div>
+          <div className="role">Front-end Developer</div>
+          <div className="company-location">
+            <div className="company">Ivory Solutions</div>
+            <div className="location">Islamabad,Pakistan</div>
+          </div>
+          <div className="job-roles">
+            <ul>
+              <li>My first Role</li>
+              <li>second </li>
+              <li>third</li>
+              <li>Fourth</li>
+            </ul>
+          </div>
+        </div>
+        <div className="shark-technology">
+          <div className="date">Feb-2024--Present</div>
+          <div className="role">Front-end Developer</div>
+          <div className="company-location">
+            <div className="company">Ivory Solutions</div>
+            <div className="location">Islamabad,Pakistan</div>
+          </div>
+          <div className="job-roles">
+            <ul>
+              <li>My first Role</li>
+              <li>second </li>
+              <li>third</li>
+              <li>Fourth</li>
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
   );
