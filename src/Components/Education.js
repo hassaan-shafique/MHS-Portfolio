0b18181377b6132fc4f3cd05615c000bdc1a524b
hassaan-shafique <hassaanshafique398@gmail.com>
2024-05-08 13:47:51 +0500

    education

--- src/Components/Education.js
+++ src/Components/Education.js
@@ -2,43 +2,45 @@ import React from 'react'
 
 const Education = () => {
   return (
-    <section id= "education"> 
-    <div>
-      <h1 className="education-head">Education </h1>
-      <div className="education">
-        <div className="school">
-          <p className="year">2014-2016</p>
-          <h3>Matric</h3>
-          <h5>Scientific Model School Islamabad,Pakistan</h5>
+    <section id="education">
+     
+        <h1 className="education-head">Education </h1>
+        <div className="education">
+          <div className="university">
+            <p className="year">2019-2023</p>
+            <h3>Bachleors in Computer Science</h3>
+            <h5>
+              Capital University of Science and Technology(CUST)
+              Islamabad,Pakistan
+            </h5>
+            <p>Relavent Course Work:</p>
+            <ul>
+              <li>Intrduction To Programming</li>
+              <li>Object Oriented Programming</li>
+              <li>Data Structure</li>
+              <li>Artificial Intelligence</li>
+              <li>Data Science</li>
+              <li>Design And Algorithm</li>
+              <li>Web Application</li>
+              <li>Cloud Computing</li>
+            </ul>
+          </div>
+          <div className="college">
+            <p className="year">2016-2018</p>
+            <h3>Pre-Engineering</h3>
+            <h5>
+              Punjab Group of College (Ayub park Campus) Islamabad,Pakistan
+            </h5>
+          </div>
+
+          <div className="school">
+            <p className="year">2014-2016</p>
+            <h3>Matric</h3>
+            <h5>Scientific Model School Islamabad,Pakistan</h5>
+          </div>
         </div>
-        <div className="college">
-          <p className="year">2016-2018</p>
-          <h3>Pre-Engineering</h3>
-          <h5>Punjab Group of College (Ayub park Campus) Islamabad,Pakistan</h5>
-        </div>
-        <div className="university">
-          <p className="year">2019-2023</p>
-          <h3>Bachleors in Computer Science</h3>
-          <h5>
-            Capital University of Science and Technology(CUST)
-            Islamabad,Pakistan
-          </h5>
-          <p>Relavent Course Work:</p>
-          <ul>
-            <li>Intrduction To Programming</li>
-            <li>Object Oriented Programming</li>
-            <li>Data Structure</li>
-            <li>Artificial Intelligence</li>
-            <li>Data Science</li>
-            <li>Design And Algorithm</li>
-            <li>Web Application</li>
-            <li>Cloud Computing</li>
-          </ul>
-        </div>
-      </div>
-    </div>
-    
-</section>
+     
+    </section>
   );
 }
 
