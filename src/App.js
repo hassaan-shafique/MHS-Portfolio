26fd188d65ad6cf58a02048823b539f00817c92d
hassaan-shafique <hassaanshafique398@gmail.com>
2024-05-09 12:45:49 +0500

    resolved erroR

---
+++ src/App.js
@@ -0,0 +1,33 @@
+
+import './App.css';
+import Header from './Components/Header';
+import Home from './Components/Home';
+import Introduction from './Components/Introduction';
+import Skills from './Components/Skills';
+import Projects from './Components/Projects';
+import Education from './Components/Education';
+import Certifications from './Components/Certifications';
+import Contact from './Components/Contact';
+import Footer from './Components/Footer';
+import Experience from './Components/Experience';
+import Blog from './Components/Blog';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Home />
+      <Introduction />
+      <Skills />
+      <Projects />
+      <Experience />
+      <Education />
+      {/* <Blog/> */}
+      <Certifications />
+      <Contact />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
