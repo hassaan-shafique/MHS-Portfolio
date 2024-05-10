04c3bd7b417c27a9183103f38a6efbd078a29eee
hassaan-shafique <hassaanshafique398@gmail.com>
2024-05-10 23:55:56 +0500

    z

--- src/Components/Experience.js
+++ src/Components/Experience.js
@@ -13,6 +13,10 @@ const Experience = () => {
           </div>
           <div className="role">
             <h3>Front-end Developer </h3>
+            <div className="skill-flex">
+              <h5>Skills:</h5>
+              <p>HTML | CSS | JavaScript | React | Tailwind Css | Firebase</p>
+            </div>
           </div>
 
           <div className="job-roles">
@@ -27,14 +31,21 @@ const Experience = () => {
         </div>
         <div className="Care">
           <div className="company-date">
-            <div className="company">Ivory Solutions</div>
-            <div className="date">Feb-2024--Present</div>
+            <div className="company">
+              Center for Advanced Research in Engineering (CARE)
+            </div>
+            <div className="date">jul 2023-Sep 2023. 3 months</div>
             <div className="location">Islamabad,Pakistan</div>
           </div>
           <div className="role">
             <h3>Front-end Developer </h3>
+            <div className="skill-flex">
+              <h5>Skills:</h5>
+              <p>HTML | CSS | JavaScript | React | Tailwind Css | Firebase</p>
+            </div>
           </div>
           <div className="job-roles">
+            <h3> Roles And Responsibilities</h3>
             <ul>
               <li>My first Role</li>
               <li>second </li>
@@ -45,37 +56,66 @@ const Experience = () => {
         </div>
         <div className="work">
           <div className="company-date">
-            <div className="company">Ivory Solutions</div>
-            <div className="date">Feb-2024--Present</div>
+            <div className="company">WORK</div>
+            <div className="date">Nov 2020-March 2021. 5 months</div>
             <div className="location">Islamabad,Pakistan</div>
           </div>
           <div className="role">
-            <h3>Front-end Developer </h3>
+            <h3> Client Service Representative </h3>
+            <div className="skill-flex">
+              <h5>Skills:</h5>
+              <p>HTML | CSS | JavaScript | React | Tailwind Css | Firebase</p>
+            </div>
           </div>
           <div className="job-roles">
+            <h3> Roles And Responsibilities</h3>
             <ul>
-              <li>My first Role</li>
-              <li>second </li>
-              <li>third</li>
-              <li>Fourth</li>
+              <li>
+                Specialized in a Home Insurance campaign, engaging with
+                customers over the phone, and effectively communicating
+                insurance information and policies.
+              </li>
+              <li>
+                Developed strong communication skills, both in voice and written
+                interactions, contributing to clear and concise customer
+                interactions.
+              </li>
+              <li>
+                Achieved fluency in English, ensuring seamless communication
+                with customers and enhancing the overall quality of customer
+                service.
+              </li>
             </ul>
           </div>
         </div>
         <div className="shark-technology">
           <div className="company-date">
-            <div className="company">Ivory Solutions</div>
-            <div className="date">Feb-2024--Present</div>
+            <div className="company">Shark technologies</div>
+            <div className="date">Dec 2018-Jun 2019. 7 months</div>
             <div className="location">Islamabad,Pakistan</div>
           </div>
           <div className="role">
-            <h3>Front-end Developer </h3>
+            <h3>Client Service Representative </h3>
+            <div className="skill-flex">
+              <h5>Skills:</h5>
+              <p>HTML | CSS | JavaScript | React | Tailwind Css | Firebase</p>
+            </div>
           </div>
           <div className="job-roles">
+            <h3> Roles And Responsibilities</h3>
             <ul>
-              <li>My first Role</li>
-              <li>second </li>
-              <li>third</li>
-              <li>Fourth</li>
+              <li>
+                Conducted outbound calls to potential customers, primarily in
+                the fields of auto insurance, home insurance, and solar energy.
+              </li>
+              <li>
+                Engaged with customers professionally, showcasing strong
+                customer interaction and communication skills.
+              </li>
+              <li>
+                Adapted quickly to changing scripts, products, and sales
+                strategies, showcasing flexibility and a rapid learning ability
+              </li>
             </ul>
           </div>
         </div>
