f9632cc18578670cf4c66615c38dd810521a9a14
hassaan-shafique <hassaanshafique398@gmail.com>
2024-05-15 23:31:40 +0500

    service

--- src/Components/Services.js
+++ src/Components/Services.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import Header from './Header'
 import { Link } from 'react-router-dom'
 
 const Services = () => {
@@ -19,6 +18,7 @@ const Services = () => {
         <input />
         <input />
         <input />
+        
       </div>
     </>
   );
