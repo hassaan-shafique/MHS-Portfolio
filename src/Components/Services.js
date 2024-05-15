9328d88b9f7ea9187574f4109ff8c753a6020941
hassaan-shafique <hassaanshafique398@gmail.com>
2024-05-15 17:01:11 +0500

    service

--- src/Components/Services.js
+++ src/Components/Services.js
@@ -5,18 +5,12 @@ import { Link } from 'react-router-dom'
 const Services = () => {
   return (
     <>
-      <h1 className="service-head caret-white ">Request a Service</h1>
+      <div className="arrow">
+        <Link to="/">
+          <img src="/left-arrow-1.svg" width={50} height={50} alt="arrow img" />
+        </Link>
+      </div>
       <div className="service-navbar">
-        <div className='arrow'>
-          <Link to="/">
-            <img
-              src="/left-arrow-1.svg"
-              width={60}
-              height={60}
-              alt="arrow img"
-            />
-          </Link>
-        </div>
         <h1 className="service-h"> Request a Service</h1>
       </div>
       <div className="service-inputs">
