8ed8e0764bfd892a392072b48fc74eaf00e3eae5
hassaan-shafique <hassaanshafique398@gmail.com>
2024-05-16 14:23:20 +0500

    form

--- src/Components/Services.js
+++ src/Components/Services.js
@@ -13,11 +13,25 @@ const Services = () => {
         <h1 className="service-h"> Request a Service</h1>
       </div>
       <div className="service-inputs">
-        <label />
-        <input type="text" />
-        <input />
-        <input />
-        <input />
+       <form class="project-form">
+    <h2 class="form-title">Project Submission Form</h2>
+    <label for="firstName" class="form-label">First Name:</label>
+    <input type="text" id="firstName" name="firstName" required class="form-input"/>
+
+    <label for="lastName" class="form-label">Last Name:</label>
+    <input type="text" id="lastName" name="lastName" required class="form-input"/>
+
+    <label for="email" class="form-label">Email Address:</label>
+    <input type="email" id="email" name="email" required class="form-input"/>
+
+    <label for="projectTitle" class="form-label">Project Title:</label>
+    <input type="text" id="projectTitle" name="projectTitle" required class="form-input"/>
+
+    <label for="projectDescription" class="form-label">Project Description:</label>
+    <textarea id="projectDescription" name="projectDescription" rows="5" required class="form-input"></textarea>
+
+    <button type="submit" class="submit-button">Submit Project</button>
+  </form>
         
       </div>
     </>
