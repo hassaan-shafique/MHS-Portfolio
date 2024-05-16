e72e09c3d844f719e9834a8c35ec27e15b02a022
hassaan-shafique <hassaanshafique398@gmail.com>
2024-05-16 12:39:03 +0500

    about btn

---
+++ src/Components/Travels.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Travels = () => {
+  return (
+    <>
+      <div className='travel-main '>
+       <div className='travel-imgs'>
+
+       </div>
+
+      </div>
+    </>
+  )
+}
+
+export default Travels
