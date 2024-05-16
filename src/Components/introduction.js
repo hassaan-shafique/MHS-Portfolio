7a0f717f1abd3c66a53c7d9d444c4f076ca646f0
hassaan-shafique <hassaanshafique398@gmail.com>
2024-05-16 12:34:54 +0500

    about

--- src/Components/introduction.js
+++ src/Components/introduction.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 const Introduction = () => {
   return (
@@ -15,24 +16,33 @@ const Introduction = () => {
           </div>
 
           <div className="introduction">
-            <h2 className='about-head'>
+            <h2 className="about-head">
               About <span>Me </span>
             </h2>
             <div className="intro-description">
               <p className="p-intro-description">
-                'I am a passionate Full stack developer with expertise in a
+                I'm a passionate
+                <strong> Full-Stack Developer</strong> with expertise in a
                 variety of technologies including HTML, CSS, JavaScript, React,
                 Next.js, Tailwind CSS, Redux, Node.js, MongoDB, and Firebase. I
-                have a strong track record of delivering high-quality projects
-                and solving complex problems. With a focus on creating
-                responsive designs and user-friendly interfaces. Explore my
-                portfolio to see some of my recent work and feel free to reach
-                out if you have any questions or would like to collaborate on a
-                project.
+                have a strong track record of delivering high-quality projects,
+                solving complex problems, and creating responsive designs with a
+                focus on user-friendly interfaces.
+                <br />
+                <strong> Beyond coding,</strong> I'm an avid traveler who loves
+                exploring new places, immersing myself in different cultures,
+                and challenging myself physically through bodybuilding.
+                <br />
+                <strong> Explore my portfolio </strong>to see some of my recent
+                work, and feel free to reach out if you have any questions or
+                would like to collaborate on a project!
               </p>
             </div>
           </div>
         </div>
+        <Link className='link-div' to=''>
+          <button className='travel-button ' role='button'> Explore my Travels</button>
+        </Link>
       </section>
     </>
   );
